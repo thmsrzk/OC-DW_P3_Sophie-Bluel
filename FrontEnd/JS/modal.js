5a5eb471d9cea2afd83caac47948e2e81467dc6b
thmsrzk <thoms.rzk@gmail.com>
2024-02-28 15:59:27 +0100

    Created the modal (empty for now), and also the modal close button (working);

--- FrontEnd/JS/modal.js
+++ FrontEnd/JS/modal.js
@@ -10,3 +10,32 @@ function removeModalRelatives() {
 }
 
 removeModalRelatives();
+
+const modal = document.querySelector("#modal");
+const stopPropagation = function (e) {
+    e.stopPropagation();
+}
+
+const openModal = function (e) {
+    e.preventDefault();
+    modal.style.display = null;
+    modal.setAttribute("aria-hidden", "false");
+    modal.setAttribute("aria-modale", 'true');
+    modal.addEventListener("click", closeModal);
+    modal.querySelector(".close-modal").addEventListener("click", closeModal);
+    modal.querySelector(".modal-wrapper").addEventListener("click", stopPropagation);
+
+}
+
+const closeModal = function (e) { 
+    e.preventDefault();
+    modal.style.display = "none";
+    modal.setAttribute("aria-hidden", "true");
+    modal.removeAttribute("aria-modale");
+    modal.removeEventListener("click", closeModal);
+    modal.querySelector(".close-modal").removeEventListener("click", closeModal);
+    modal.querySelector(".modal-wrapper").removeEventListener("click", stopPropagation);
+
+}
+
+document.querySelector(".open-modal").addEventListener("click", openModal);
